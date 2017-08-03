62b79ba30245c76e9a1e06a92eac87815a52ecda
diewoo <dgo250594@gmail.com>
2017-08-02 22:28:06 -0500

    logica

--- db/connection.js
+++ db/connection.js
@@ -4,46 +4,46 @@ const mode = config.mode;
 const db = config[mode].db;
 
 const pool = mysql.createPool({
-    connectionLimit: db.pool,
-    host: db.host,
-    port: db.port,
-    user: db.user,
-    password: db.pwd,
-    database: db.name,
-    multipleStatements: true
+  connectionLimit: db.pool,
+  host: db.host,
+  port: db.port,
+  user: db.user,
+  password: db.pwd,
+  database: db.name,
+  multipleStatements: true
 });
 
 function getConnection(cb) {
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            return cb(err);
-        }
-        cb(null, connection);
-    });
+  pool.getConnection(function(err, connection) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, connection);
+  });
 };
 
 function commit(connection, callback) {
-    connection.commit(function (err) {
-        if (err) {
-            return connection.rollback(function () {
-                connection.release();
-                callback(err);
-            });
-        } else {
-            connection.release();
-            callback(null);
-        }
-    });
+  connection.commit(function(err) {
+    if (err) {
+      return connection.rollback(function() {
+        connection.release();
+        callback(err);
+      });
+    } else {
+      connection.release();
+      callback(null);
+    }
+  });
 }
 
 function rollback(connection) {
-    connection.rollback(function () {
-        connection.release();
-    });
+  connection.rollback(function() {
+    connection.release();
+  });
 }
 
 module.exports = {
-    getConnection,
-    rollback,
-    commit
+  getConnection,
+  rollback,
+  commit
 };
